4ece7ba1d26f6117a05eed878bee2559498813c9
bruce5283 <bcarrington7@gmail.com>
2022-07-09 11:27:14 -0400

    Updated

--- function.js
+++ function.js
@@ -113,7 +113,7 @@ function playBuffer() {
 function onend() {
   document.getElementById("pre").style.color ="#A81248";
   document.getElementById("pre").innerHTML = "NEXT MOVEMENT";
-  time = ${time};
+  time = "";
   btn.disabled = false;
 }
 
